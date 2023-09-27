0d2bf8d2f0bc05932a9e2c2b1f34a4106c38c5e8
Paula Gearon <pgearon@imohealth.com>
2023-09-27 13:01:12 -0400

    Bumped the version

--- build.clj
+++ build.clj
@@ -4,7 +4,7 @@
             [org.corfield.build :as bb]))
 
 (def lib 'org.clojars.quoll/spike)
-(def version "0.0.1")
+(def version "0.0.2")
 
 ;; clojure -T:build test
 (defn test "Run the tests." [opts]
